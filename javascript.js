574a345e31a2d41fe5096fb30842e495223e917b
Abbi Nizar <abbinizarm@gmail.com>
2020-01-02 17:44:47 +0700

    getelement

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var ct = document.getElementById("title");
+// a.getAttribute("type");
+// a.getAttribute("name");
+
+var headings = document.getElementsByClassName("heading");
+headings[0];
+headings[1];
